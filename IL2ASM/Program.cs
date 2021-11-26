28dedbe211f43657bc88a2367b1d0a9de55b14cf
nifanfa <nifanfa@foxmail.com>
2021-11-26 20:07:44 +0800

    Use Dot Net 3.1

--- IL2ASM/Program.cs
+++ IL2ASM/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace IL2ASM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
