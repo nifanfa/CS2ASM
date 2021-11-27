3d2a6fbf45958b20608c5dd94d39ea3f433042b4
nifanfa <nifanfa@foxmail.com>
2021-11-27 15:54:50 +0800

    Update Grub2

--- IL2ASM/Amd64/LongMode.asm
+++ IL2ASM/Amd64/LongMode.asm
@@ -119,6 +119,11 @@ _Main:
     mov gs, ax
     mov ss, ax
 
+    mov rax,0x0f20
+    mov rdi,0xb8000
+    mov rcx,0xfa0
+    rep stosw
+
     mov rbp, rsp
     
     %include "Kernel.asm"
--- IL2ASM/Program.cs
+++ IL2ASM/Program.cs
@@ -46,9 +46,9 @@ namespace IL2ASM
                     case ProcessorArchitecture.Amd64:
                         File.WriteAllText(@"Amd64\Kernel.asm", Arch._Code.ToString());
 
-                        Util.Start(@"Amd64\nasm.exe", "-fbin Multiboot.asm -o main.exe");
+                        Util.Start(@"Amd64\nasm.exe", "-fbin Multiboot.asm -o kernel");
                         ZipFile.ExtractToDirectory(@"Amd64\grub2.zip", @"Amd64\Temp\", true);
-                        File.Move(@"Amd64\main.exe", @"Amd64\Temp\boot\main.exe", true);
+                        File.Move(@"Amd64\kernel", @"Amd64\Temp\boot\kernel", true);
                         Util.Start(@"Amd64\mkisofs.exe", $"-relaxed-filenames -J -R -o \"{Environment.CurrentDirectory}\\output.iso\" -b \"{@"boot/grub/i386-pc/eltorito.img"}\" -no-emul-boot -boot-load-size 4 -boot-info-table {Environment.CurrentDirectory}\\Amd64\\Temp");
                         Process.Start($"{Environment.CurrentDirectory}\\Amd64QEMU.bat");
                         break;
