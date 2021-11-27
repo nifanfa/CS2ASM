dae4d3fd9da5e25cfd6d3ad404a582dc083bb5b2
nifanfa <nifanfa@foxmail.com>
2021-11-27 18:08:09 +0800

    Update ConsoleApp1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp1
+{
+    public static class Program
+    {
+        public static unsafe void Main()
+        {
+            byte* p = (byte*)0xb8000;
+
+            *p = (byte)'H';
+            p += 2;
+            *p = (byte)'e';
+            p += 2;
+            *p = (byte)'l';
+            p += 2; 
+            *p = (byte)'l';
+            p += 2;
+            *p = (byte)'o';
+            p += 2;
+            *p = (byte)' ';
+            p += 2;
+            *p = (byte)'L';
+            p += 2;
+            *p = (byte)'e';
+            p += 2;
+            *p = (byte)'o';
+            p += 2;
+            *p = (byte)'n';
+        }
+    }
+}
--- IL2ASM/Program.cs
+++ IL2ASM/Program.cs
@@ -24,7 +24,7 @@ namespace IL2ASM
                 }
                 arch.Setup();
 
-                ModuleDefMD def = ModuleDefMD.Load(@"C:\Users\nifan\source\repos\ConsoleApp1\ConsoleApp1\bin\Debug\net5.0\ConsoleApp1.dll");
+                ModuleDefMD def = ModuleDefMD.Load(@"..\..\..\..\ConsoleApp1\bin\Debug\netcoreapp3.1\ConsoleApp1.dll");
                 Stopwatch stopwatch = new Stopwatch();
                 stopwatch.Start();
 
