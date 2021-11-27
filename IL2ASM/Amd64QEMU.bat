063c360d585c5787974cc69730d1813649ae27c2
nifanfa <nifanfa@foxmail.com>
2021-11-27 14:00:39 +0800

    Update Amd64QEMU.bat

--- IL2ASM/Amd64QEMU.bat
+++ IL2ASM/Amd64QEMU.bat
@@ -1,1 +1,9 @@
-"C:\Program Files\qemu\qemu-system-x86_64.exe" -m 8192 -cdrom output.iso
+@echo off
+set qemu="C:\Program Files\qemu\qemu-system-x86_64.exe"
+if not exist %qemu% (
+echo QEMU is not installed
+pause
+exit
+)else (
+%qemu% -m 8192 -cdrom output.iso
+)
