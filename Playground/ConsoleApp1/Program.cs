461a72c23813991329401bcf18975f967f799433
LeonTheDev <leonrobinson.dev@gmail.com>
2021-12-02 18:51:38 +0000

    Update Program.cs

--- Playground/ConsoleApp1/Program.cs
+++ Playground/ConsoleApp1/Program.cs
@@ -2,141 +2,136 @@
 {
     public static unsafe class Program
     {
-        public static ulong Position = 0;
+
+        // Colors
+        private const byte Black = 0x00;
+        private const byte Blue = 0x01;
+        private const byte Green = 0x02;
+        private const byte Cyan = 0x03;
+        private const byte Red = 0x04;
+        private const byte Purple = 0x05;
+        private const byte Brown = 0x06;
+        private const byte Gray = 0x07;
+        private const byte DarkGray = 0x08;
+        private const byte LightBlue = 0x09;
+        private const byte LightGreen = 0x0A;
+        private const byte LightCyan = 0x0B;
+        private const byte LightRed = 0x0C;
+        private const byte LightPurple = 0x0D;
+        private const byte Yellow = 0x0E;
+        private const byte White = 0x0F;
+
+        private const byte Width = 80;
+        private const byte Height = 25;
+
+        private static byte Color = 0;
+        private static ulong Position = 0;
 
         /*
          * Current Progress:
-         *  Static Methods Only
-         *  Local And Static Variable Only
-         *  Pointer Support
-         *  Sbyte Byte Short UShort Int Uint Long ULong Support
-         *  C# basic things(for if) etc Support
+         *  Only static methods supported.
+         *  Only local and static variables supported.
+         *  Pointers supported.
+         *  SByte, Byte, Short, UShort, Int, UInt, Long, ULong, are all supported.
+         *  C# basic things (for, if, etc, return, and more) supported.
          *  
-         *  Allocation Is Not Supported
-         *  Return Not Work. Check out Ret.cs and Call.cs
+         *  Allocation is not yet supported!
          */
         public static void Main()
         {
-            byte b = Ret();
-            PutChar((char)b);
+            SetupConsole();
+            AllSymbols();
         }
 
-        public static byte Ret() { return 0x41; }
-
-        public static void PutChar(char chr)
+        private static void Alphabet()
         {
-            byte* p = (byte*)(0xb8000 + Position);
+            byte b = 0x41; // Start at 0x41 (character A)
+            for (int i = 0; i < 26; i++) // Loop through next 25 characters after A
+            {
+                Write((char)b);
+                b++;
+            }
+        }
+
+        private static void AllSymbols()
+        {
+            byte b = 0x01; // Start at 0x01
+            for (int i = 0; i < 256; i++)
+            {
+                Write((char)b);
+                b++;
+            }
+        }
+
+        public static void SetupConsole()
+        {
+            BackgroundColor = Black;
+            ForegroundColor = White;
+            Clear();
+        }
+
+        public static void Write(char chr)
+        {
+            byte* p = GetVideoAddress();
             *p = (byte)chr;
-            Position = Position + 2;
+            p++;
+            *p = Color;
+            Position += 2;
         }
-        
-        /*
-        public static void Main()
+
+        public static void WriteF(char chr, byte fg)
         {
-            if (Position == 0)
-            {
-                PutChar('Y');
-            }
-            else
-            {
-                PutChar('N');
-            }
-
-            if (Position == 1)
-            {
-                PutChar('Y');
-            }
-            else
-            {
-                PutChar('N');
-            }
+            byte pfg = ForegroundColor;
+            ForegroundColor = fg;
+            Write(chr);
+            ForegroundColor = pfg;
         }
-        */
 
-        /*
-        public static void Main()
+        public static void WriteB(char chr, byte bg)
         {
-            bool T = true;
-            if (T)
-            {
-                PutChar('T');
-            }
-            else
-            {
-                PutChar('F');
-            }
-
-            bool F = false;
-            if (F)
-            {
-                PutChar('T');
-            }
-            else
-            {
-                PutChar('F');
-            }
+            byte pbg = BackgroundColor;
+            BackgroundColor = bg;
+            Write(chr);
+            BackgroundColor = pbg;
         }
-        */
 
-        /*
-        public static void ArguTest1(byte b1,byte b2)
+        public static void WriteFB(char chr, byte fg, byte bg)
         {
-            byte* p0 = (byte*)0xb8000;
-            *p0 = b1;
-
-            byte* p1 = (byte*)0xb8002;
-            *p1 = b2;
+            byte bfg = ForegroundColor;
+            byte pbg = BackgroundColor;
+            ForegroundColor = fg;
+            BackgroundColor = bg;
+            Write(chr);
+            ForegroundColor = bfg;
+            BackgroundColor = pbg;
         }
 
-        public static void Main()
+        public static void Clear()
         {
-        Loop:
-            Second();
-            byte b = ReturnTest();
-            byte* p = (byte*)0xb8000;
-            *p = b;
-
-            ArguTest(0x42);
-
-            //ArguTest1(0x41, 0x42);
-
-            goto Loop;
+            Position = 0;
+            int Res = Width * Height;
+            for (int i = 0; i < Res; i++)
+            {
+                Write(' ');
+            }
+            Position = 0;
         }
 
-        public static void ArguTest(byte b) 
+        public static byte ForegroundColor
         {
-            byte* p = (byte*)0xb8002;
-            *p = b;
+            get { return (byte)(Color & 0x0F); }
+            set { Color &= 0xF0; Color |= (byte)(value & 0x0F); }
         }
 
-        public static byte ReturnTest() { return 0x41; }
-
-        public static void Second()
+        public static byte BackgroundColor
         {
-            byte* p = (byte*)0xb8000;
-
-            *p = (byte)'H';
-            p += 2;
-            *p = (byte)'e';
-            p += 2;
-            *p = (byte)'l';
-            p += 2;
-            *p = (byte)'l';
-            p += 2;
-            *p = (byte)'o';
-            p += 2;
-            *p = (byte)' ';
-            p += 2;
-            *p = (byte)'W';
-            p += 2;
-            *p = (byte)'o';
-            p += 2;
-            *p = (byte)'r';
-            p += 2;
-            *p = (byte)'l';
-            p += 2;
-            *p = (byte)'d';
+            get { return (byte)(Color >> 4); }
+            set { Color &= 0x0F; Color |= (byte)((value & 0x0F) << 4); }
         }
-        */
+
+        private static byte* GetVideoAddress()
+        {
+            return (byte*)(0xb8000 + Position);
+        }
     }
 }
