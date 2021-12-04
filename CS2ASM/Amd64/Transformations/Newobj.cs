389b8e4e63d2c7c3430742c10efaca50f8a3404a
nifanfa <nifanfa@foxmail.com>
2021-12-05 02:56:17 +0800

    Update Newobj.cs

--- CS2ASM/Amd64/Transformations/Newobj.cs
+++ CS2ASM/Amd64/Transformations/Newobj.cs
@@ -14,6 +14,7 @@ namespace CS2ASM
             arch.Append($"push rax");
             arch.Append($"call {Amd64.SafeMethodName((MethodDef)ins.Operand)}");
             arch.Append($"pop rbp");
+            //arch.Append($"add rsp,8"); //This will be called by stloc so we don't need to do it here
         }
     }
 }
