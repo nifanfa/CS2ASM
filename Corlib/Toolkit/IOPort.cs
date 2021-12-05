e336909b7a4fa9517168c6eef939de0f15837d79
LeonTheDev <leonrobinson.dev@gmail.com>
2021-12-05 15:58:37 +0000

    Update IOPort

--- Corlib/Toolkit/IOPort.cs
+++ Corlib/Toolkit/IOPort.cs
@@ -4,6 +4,16 @@ namespace Toolkit
 {
     public static class IOPort
     {
+        public static byte In8(ushort port)
+        {
+            byte data = 0;
+            asm("mov rdx,{port}");
+            asm("xor rax,rax");
+            asm("in al,dx");
+            asm("mov {data},al");
+            return data;
+        }
+
         public static void Out16(ushort port, ushort value)
         {
             asm("mov rdx,{port}");
--- Playground/ConsoleApp1/Program.cs
+++ Playground/ConsoleApp1/Program.cs
@@ -22,7 +22,7 @@ namespace ConsoleApp1
         public static void Main()
         {
             //shutdown qemu
-            //Out16(0x604, 0x2000);
+            IOPort.Out16(0x604, 0x2000);
 
             //In8(0x60);
 
@@ -44,23 +44,6 @@ namespace ConsoleApp1
             //Warning: Those string won't get disposed
 
             Counter();
-        }
-
-        public static byte In8(ushort port)
-        {
-            byte data = 0;
-            asm("mov rdx,{port}");
-            asm("xor rax,rax");
-            asm("in al,dx");
-            asm("mov {data},al");
-            return data;
-        }
-
-        public static void Out16(ushort port,ushort value)
-        {
-            asm("mov rdx,{port}");
-            asm("mov rax,{value}");
-            asm("out dx,ax");
         }
 
         // Counts to 9 and resets
