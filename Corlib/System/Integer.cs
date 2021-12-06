be3a1237356eeca828a12d687c3e9e92e5c208de
LeonTheDev <69083497+LeonTheDev@users.noreply.github.com>
2021-12-06 17:14:25 +0000

    Integer.GetLength

--- Corlib/System/Integer.cs
+++ Corlib/System/Integer.cs
@@ -12,6 +12,20 @@ namespace System
             }
             return firstDigit;
         }
+        
+        public static int GetLength(int x)
+        {
+            if (x >= 1000000000) return 10;
+            if (x >= 100000000) return 9;
+            if (x >= 10000000) return 8;
+            if (x >= 1000000) return 7;
+            if (x >= 100000) return 6;
+            if (x >= 10000) return 5;
+            if (x >= 1000) return 4;
+            if (x >= 100) return 3;
+            if (x >= 10) return 2;
+            return 1;
+        }
 
         public static int GetLastDigit(int i)
         {
