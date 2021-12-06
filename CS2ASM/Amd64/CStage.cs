547156b0a88f2b9f9876159654de21f92b2aea76
nifanfa <nifanfa@foxmail.com>
2021-12-07 04:08:49 +0800

    Update CStage.cs

--- CS2ASM/Amd64/CStage.cs
+++ CS2ASM/Amd64/CStage.cs
@@ -8,16 +8,16 @@ namespace CS2ASM.AMD64
 {
     public static class CStage
     {
-        public static unsafe void CompileC(ModuleDefMD def, BaseArch arch, string fileNameNoExtensionAndPath)
+        public static unsafe void CompileC(ModuleDefMD def, BaseArch arch, string fileNameNoExtensionNameAndPath)
         {
             if (!File.Exists(@"C:\mingw64\bin\gcc.exe"))
             {
                 Utility.Start(@"Amd64\mingw64.exe", "-o\"C:\\\" -y");
             }
-            Utility.Start(@"C:\mingw64\bin\gcc.exe", new FileInfo(def.Location).DirectoryName, @$"gcc -fno-asynchronous-unwind-tables -O2 -s -c -o {fileNameNoExtensionAndPath}.o {fileNameNoExtensionAndPath}.c");
-            File.Move(new FileInfo(def.Location).DirectoryName + @$"\{fileNameNoExtensionAndPath}.o", @$"Amd64\{fileNameNoExtensionAndPath}.o", true);
-            Utility.Start(@"Amd64\objconv-x64.exe", $"-fnasm {fileNameNoExtensionAndPath}.o");
-            StreamReader str = new StreamReader(@$"Amd64\{fileNameNoExtensionAndPath}.asm");
+            Utility.Start(@"C:\mingw64\bin\gcc.exe", new FileInfo(def.Location).DirectoryName, @$"gcc -fno-asynchronous-unwind-tables -O2 -s -c -o {fileNameNoExtensionNameAndPath}.o {fileNameNoExtensionNameAndPath}.c");
+            File.Move(new FileInfo(def.Location).DirectoryName + @$"\{fileNameNoExtensionNameAndPath}.o", @$"Amd64\{fileNameNoExtensionNameAndPath}.o", true);
+            Utility.Start(@"Amd64\objconv-x64.exe", $"-fnasm {fileNameNoExtensionNameAndPath}.o");
+            StreamReader str = new StreamReader(@$"Amd64\{fileNameNoExtensionNameAndPath}.asm");
             string line = string.Empty;
             while ((line = str.ReadLine()) != null)
             {
