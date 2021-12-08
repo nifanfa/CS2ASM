82d05d8f32880178f2bd74aab10be85724c1fe2c
nifanfa <nifanfa@foxmail.com>
2021-12-09 00:41:32 +0800

    Update Amd64.cs

--- CS2ASM/Amd64/Amd64.cs
+++ CS2ASM/Amd64/Amd64.cs
@@ -1,6 +1,7 @@
 ﻿/*
  * Reference: 
  * https://godbolt.org/
+ * https://sharplab.io/
  * http://www.cs.albany.edu/~sdc/CSI500/Fal13/Readings/CSPPCh3.pdf
  * https://docs.microsoft.com/en-us/windows-hardware/drivers/debugger/x64-architecture
  * https://docs.microsoft.com/zh-cn/dotnet/api/system.reflection.emit.opcodes?view=net-6.0
