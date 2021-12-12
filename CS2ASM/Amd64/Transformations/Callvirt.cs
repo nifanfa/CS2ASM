3f637306fa9adadc21e6a3305c58f10b880fdb33
nifanfa <nifanfa@foxmail.com>
2021-12-12 22:33:15 +0800

    Update Callvirt.cs

--- CS2ASM/Amd64/Transformations/Callvirt.cs
+++ CS2ASM/Amd64/Transformations/Callvirt.cs
@@ -10,7 +10,6 @@ namespace CS2ASM
         public static void Callvirt(BaseArch arch, Instruction ins, MethodDef def)
         {
             Call(arch, ins, def);
-            //throw new NotImplementedException("Callvirt is not implemented");
         }
     }
 }
