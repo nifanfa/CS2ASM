72d484000d3c6b98105cc7e3b5056db59eb4f220
nifanfa <nifanfa@foxmail.com>
2021-12-15 00:28:34 +0800

    Update Array.cs

--- Corlib/System/Array.cs
+++ Corlib/System/Array.cs
@@ -5,6 +5,8 @@
         public static object Ctor(int length)
         {
             Array array = new Array();
+
+            //Fill the memory area check out Stelem.cs
             ulong* p = stackalloc ulong[length];
 
             return array;
