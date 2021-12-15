f64f51ce36d6215cd747da5f3d47c74ebe40220c
LeonTheDev <leonrobinson.dev@gmail.com>
2021-12-15 17:12:03 +0000

    Start implementing GC

---
+++ Corlib/System.Runtime/GC.cs
@@ -0,0 +1,17 @@
+﻿
+namespace System.Runtime
+{
+    public static unsafe class GC
+    {
+        public struct MemoryDescriptor
+        {
+            public uint Address;
+            public uint Size;
+        }
+
+        private static Pointer AllocateMemory(uint size)
+        {
+            return Pointer.Zero;
+        }
+    }
+}
