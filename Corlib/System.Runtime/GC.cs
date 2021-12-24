7123e82672b25a326fded3bc8cf973907f5acb7b
nifanfa <nifanfa@foxmail.com>
2021-12-25 00:09:21 +0800

    Update GC.cs

--- Corlib/System.Runtime/GC.cs
+++ Corlib/System.Runtime/GC.cs
@@ -10,12 +10,39 @@ namespace System.Runtime
             public ulong Address;
             public ulong Size;
         }
+        private static MemoryDescriptor* MDs = null;
+        private const int MDCount = 1024;
 
         public static ulong HeapStart = 10 * 1024 * 1024;
         public static ulong Allocation = 0;
 
+        static GC() 
+        {
+            MemoryDescriptor* desc = stackalloc MemoryDescriptor[MDCount];
+            MDs = desc;
+
+            for(int i = 0; i < MDCount; i++) 
+            {
+                desc[i].Address = 0;
+                desc[i].Size = 0;
+            }
+        }
+
         public static ulong Allocate(ulong size)
         {
+            if(MDs != null)
+            {
+                for (ulong i = 0; i < MDCount; i++)
+                {
+                    if (MDs[i].Size >= size)
+                    {
+                        MDs[i].Address = MDs[i].Address + size;
+                        MDs[i].Size = MDs[i].Size - size;
+                        return MDs[i].Address;
+                    }
+                }
+            }
+
             Allocation++;
             ulong ptr = HeapStart;
             HeapStart = HeapStart + size;
@@ -26,9 +53,18 @@ namespace System.Runtime
         {
             ulong p = Unsafe.AddressOf(obj);
             ulong size = obj.Size;
+
+            for (int i = 0; i < MDCount; i++)
+            {
+                if (MDs[i].Size == 0)
+                {
+                    MDs[i].Size = size;
+                    MDs[i].Address = p;
+                }
+            }
+
             asm("mov rdi,{p}");
             asm("xor rax,rax");
-            asm("not rax");
             asm("mov rcx,{size}");
             asm("rep stosb");
         }
