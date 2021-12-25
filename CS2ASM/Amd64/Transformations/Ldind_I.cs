98b5b45e03f56f6fef30b31efe54f832bc8183e2
nifanfa <nifanfa@foxmail.com>
2021-12-26 00:04:12 +0800

    Optimization

--- CS2ASM/Amd64/Transformations/Ldind_I.cs
+++ CS2ASM/Amd64/Transformations/Ldind_I.cs
@@ -1,5 +1,6 @@
 using dnlib.DotNet;
 using dnlib.DotNet.Emit;
+using System.Diagnostics;
 
 namespace CS2ASM
 {
@@ -8,6 +9,25 @@ namespace CS2ASM
         [ILTransformation(Code.Ldind_I)]
         public static void Ldind_I(BaseArch arch, Instruction ins, MethodDef def)
         {
+            var prev = def.Body.Instructions[def.Body.Instructions.IndexOf(ins) - 1];
+            if (prev.IsLdloc())
+            {
+                var p = def.Body.Variables[(int)OperandParser.Ldloc(prev)];
+                if (!p.Type.IsPointer)
+                {
+                    return;
+                }
+            }
+            if (prev.IsLdarg())
+            {
+                var p = def.Parameters[(int)OperandParser.Ldarg(prev)];
+                if (!p.Type.IsPointer)
+                {
+                    return;
+                }
+            }
+
+            Debugger.Break();
             arch.Append($"pop rax");
             arch.Append($"push qword [rax]");
         }
--- CS2ASM/Amd64/Transformations/Ldind_U1.cs
+++ CS2ASM/Amd64/Transformations/Ldind_U1.cs
@@ -8,6 +8,24 @@ namespace CS2ASM
         [ILTransformation(Code.Ldind_U1)]
         public static void Ldind_U1(BaseArch arch, Instruction ins, MethodDef def)
         {
+            var prev = def.Body.Instructions[def.Body.Instructions.IndexOf(ins) - 1];
+            if (prev.IsLdloc())
+            {
+                var p = def.Body.Variables[(int)OperandParser.Ldloc(prev)];
+                if (!p.Type.IsPointer)
+                {
+                    return;
+                }
+            }
+            if (prev.IsLdarg())
+            {
+                var p = def.Parameters[(int)OperandParser.Ldarg(prev)];
+                if (!p.Type.IsPointer)
+                {
+                    return;
+                }
+            }
+
             arch.Append($"pop rax");
             arch.Append($"push qword [rax]");
         }
