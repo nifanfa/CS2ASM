8f7b7dfb26a0c80a82e6b4d8fd5d8f6a3b62e78a
nifanfa <nifanfa@foxmail.com>
2021-12-31 17:41:26 +0800

    Comment Call.cs

--- CS2ASM/Amd64/Transformations/Call.cs
+++ CS2ASM/Amd64/Transformations/Call.cs
@@ -14,13 +14,14 @@ namespace CS2ASM
             var prevIns = def.Body.Instructions[def.Body.Instructions.IndexOf(ins) - 1];
             if(prevIns.OpCode.Code == Code.Ldstr) 
             {
-                Console.WriteLine($"Warning: The string \"{prevIns.Operand}\" will be disposed automatically (call from Call.cs)");
-                arch.Append("push qword [rsp]");
+                //It crashes VirtualBox but works in qemu
+                //Console.WriteLine($"Warning: The string \"{prevIns.Operand}\" will be disposed automatically (call from Call.cs)");
+                //arch.Append("push qword [rsp]");
             }
             arch.Append($"call {Utility.SafeMethodName(((MethodDef)ins.Operand))}");
             if (prevIns.OpCode.Code == Code.Ldstr)
             {
-                arch.Append("call System.Object.Dispose");
+                //arch.Append("call System.GC.Dispose.Object");
             }
         }
     }
