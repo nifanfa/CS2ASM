a375cf261b44bafee7d0852eadc1d0d98f57c0af
nifanfa <nifanfa@foxmail.com>
2022-01-13 00:20:52 +0800

    Update Call.cs

--- CS2ASM/Amd64/Transformations/Call.cs
+++ CS2ASM/Amd64/Transformations/Call.cs
@@ -11,6 +11,41 @@ namespace CS2ASM
         [ILTransformation(Code.Call)]
         public static void Call(BaseArch arch, Instruction ins, MethodDef def)
         {
+            int variCount = 0;
+            if (ins.Operand is MemberRef)
+                variCount = ((MemberRef)ins.Operand).MethodSig.Params.Count;
+            else
+                variCount = ((MethodDef)ins.Operand).MethodSig.Params.Count;
+            
+            //This is for calling c/c++ code
+            if(variCount <= 6)
+            {
+                if (variCount >= 1)
+                {
+                    arch.Append($"mov rdi,[rsp+{(variCount - 1) * 8}]");
+                }
+                if (variCount >= 2)
+                {
+                    arch.Append($"mov rsi,[rsp+{(variCount - 2) * 8}]");
+                }
+                if (variCount >= 3)
+                {
+                    arch.Append($"mov rdx,[rsp+{(variCount - 3) * 8}]");
+                }
+                if (variCount >= 4)
+                {
+                    arch.Append($"mov rcx,[rsp+{(variCount - 4) * 8}]");
+                }
+                if (variCount >= 5)
+                {
+                    arch.Append($"mov r8,[rsp+{(variCount - 5) * 8}]");
+                }
+                if (variCount >= 6)
+                {
+                    arch.Append($"mov r9,[rsp+{(variCount - 6) * 8}]");
+                }
+            }
+
             var prevIns = def.Body.Instructions[def.Body.Instructions.IndexOf(ins) - 1];
             if(prevIns.OpCode.Code == Code.Ldstr) 
             {
