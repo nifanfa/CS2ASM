8c2735a80281ca3b6c6f566075380120bce6ceaf
nifanfa <nifanfa@foxmail.com>
2022-01-15 16:28:20 +0800

    Update PageTable.cs

--- System.Platform.Amd64/PageTable.cs
+++ System.Platform.Amd64/PageTable.cs
@@ -10,7 +10,7 @@ namespace System.Platform.Amd64
             Huge = 0x200000
         }
 
-        public static ulong* PML4 = (ulong*)0x3FE000;
+        public static ulong* PML4 = (ulong*)0x2FE000;
 
         static PageTable()
         {
@@ -83,7 +83,7 @@ namespace System.Platform.Amd64
 
         public static ulong* AllocateTable()
         {
-            ulong* r = (ulong*)(0x400000 + (p * 4096));
+            ulong* r = (ulong*)(0x300000 + (p * 4096));
             Native.Stosb(r, 0x00, 4096);
             p++;
             return r;
