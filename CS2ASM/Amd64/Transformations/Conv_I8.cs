9e2bbf7c4835eed6e70be16568e70a9367649c8e
nifanfa <nifanfa@foxmail.com>
2022-01-16 22:10:09 +0800

    Update Conv_I8.cs

--- CS2ASM/Amd64/Transformations/Conv_I8.cs
+++ CS2ASM/Amd64/Transformations/Conv_I8.cs
@@ -8,11 +8,6 @@ namespace CS2ASM
         [ILTransformation(Code.Conv_I8)]
         public static void Conv_I8(Context context)
         {
-            context.Append($"xor rdx,rdx");
-            context.Append($"mov rbx,0xFFFFFFFFFFFFFFFF");
-            context.Append($"pop rax");
-            context.Append($"and rax,rbx");
-            context.Append($"push rax");
         }
     }
 }
