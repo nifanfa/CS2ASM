756469d6841728609c7b5cbf5d48532a1b9109f9
nifanfa <nifanfa@foxmail.com>
2022-01-17 12:31:46 +0800

    Update EntryPoint.asm

--- CS2ASM/Amd64/EntryPoint.asm
+++ CS2ASM/Amd64/EntryPoint.asm
@@ -90,20 +90,20 @@ dq 0
 %endmacro
 
 %macro popaq 0
-	push r15
-	push r14
-	push r13
-	push r12
-	push r11
-	push r10
-	push r9
-	push r8
-	push rdi
-	push rsi
-	push rbx
-	push rdx
-	push rcx
-	push rax
+	pop rax
+	pop rcx
+	pop rdx
+	pop rbx
+	pop rsi
+	pop rdi
+	pop r8
+	pop r9
+	pop r10
+	pop r11
+	pop r12
+	pop r13
+	pop r14
+	pop r15
 %endmacro
 
 ALIGN 4
