4f0dd5b4410225a700a4dabf9a7969991e90d150
nifanfa <nifanfa@foxmail.com>
2022-01-17 14:34:50 +0800

    添加 .gitignore 和 .gitattributes。

---
+++ System.Private.Corlib/Runtime/InteropServices/CallingConvention.cs
@@ -0,0 +1,13 @@
+// Copyright (c) MOSA Project. Licensed under the New BSD License.
+
+namespace System.Runtime.InteropServices
+{
+	public enum CallingConvention
+	{
+		Winapi = 1,
+		Cdecl = 2,
+		StdCall = 3,
+		ThisCall = 4,
+		FastCall = 5,
+	}
+}
