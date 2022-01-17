2851778eac0081e97522ea48decefba90d19862f
nifanfa <nifanfa@foxmail.com>
2022-01-17 12:29:48 +0800

    Update EntryPoint.asm

--- CS2ASM/Amd64/EntryPoint.asm
+++ CS2ASM/Amd64/EntryPoint.asm
@@ -73,41 +73,37 @@ multiboot_ptr:
 dq 0
 
 %macro pushaq 0
-push rax
-push rcx
-push rdx
-push rbx
-push rsp
-push rbp
-push rsi
-push rdi
-push r8
-push r9
-push r10
-push r11
-push r12
-push r13
-push r14
-push r15
+	push r15
+	push r14
+	push r13
+	push r12
+	push r11
+	push r10
+	push r9
+	push r8
+	push rdi
+	push rsi
+	push rbx
+	push rdx
+	push rcx
+	push rax
 %endmacro
 
 %macro popaq 0
-pop r15
-pop r14
-pop r13
-pop r12
-pop r11
-pop r10
-pop r9
-pop r8
-pop rdi
-pop rsi
-pop rbp
-pop rsp
-pop rbx
-pop rdx
-pop rcx
-pop rax
+	push r15
+	push r14
+	push r13
+	push r12
+	push r11
+	push r10
+	push r9
+	push r8
+	push rdi
+	push rsi
+	push rbx
+	push rdx
+	push rcx
+	push rax
 %endmacro
 
 ALIGN 4
